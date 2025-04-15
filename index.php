b70c1fe9440dbd2708e9dfe3296670af1c9b4b36
Aladdin A. Buwanding <buwandingaladdin@gmail.com>
2025-04-15 16:04:21 +0800

    making the set up 04-15-25

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h6>
+
+        </h6>
+    </div>
+    <table>
+        <thead>
+            <th></th>
+        </thead>
+        
+    </table>
+</body>
+</html>
