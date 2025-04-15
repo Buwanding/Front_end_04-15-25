8dd2a71e32c222a0eef646fb7816c366e48b3bff
Aladdin Buwanding <125986823+Buwanding@users.noreply.github.com>
2025-04-15 16:18:59 +0800

    Add files via upload
    making the set up 04-15-25

---
+++ Client/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student List</title>
+    <link rel="stylesheet" href="indexstyle.css">
+</head>
+
+<body>
+    <div>
+        <h6>List of Students</h6>
+    </div>
+    <table>
+        <thead>
+            <tr>
+                <th>Student Name</th>
+                <th>Email</th>
+                <th colspan="2">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Aladdin</td>
+                <td>aladdin@example.com</td>
+                <td><button class="action-btn edit">Edit</button></td>
+                <td><button class="action-btn delete">Delete</button></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
